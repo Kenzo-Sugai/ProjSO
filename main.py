9118bb01f0539a47ab1f178f9c62206720eb8574
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2023-10-18 22:13:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import queue
 
-def readfile(processos):
+def readfile():
+
+    processosLeitura = {}
+
     arq = open("entrada.txt")
     linhas = arq.readlines()
     for linha in linhas:
         partes = linha.strip().split()
         fila = queue.Queue()
-        for i in range(1,len(partes)):
-            fila.put(int(partes[i]))
-        processos[partes[0]] = fila
+        if len(partes) == 4:
+            numeros = partes[3].split(",")
+            for i in numeros:
+                fila.put(int(i))
+        processosLeitura[partes[0]] = [int(partes[1]), int(partes[2]),fila]
 
-    return processos
+    return dict(sorted(processosLeitura.items(), key=lambda item: item[1][1]))
+
+
+def removerDic(remover, processos):
+    for i in remover:
+            if(processos[i][0] == 0):
+                processos.pop(i)
+
+def getFila(processos):
+    Fila = queue.Queue()
+
+    for i in processos:
+        Fila.put([i, processos[i]])
+
+    return Fila
 
 def roundRobin(processos, Quantum):
+
+    processosFila = getFila(processos)
 
     print("""
 -----------------------------------
@@ -21,48 +42,68 @@ def roundRobin(processos, Quantum):
     """)
 
     tempo = 0
+    IO = 0
+    
+    Fila = queue.Queue()
 
-    while(len(processos) > 0):
+    Fila.put(processosFila.get())
 
-        print(f"********** TEMPO {tempo} **************")
+    while(Fila.not_empty):
 
         remover = []
-        QuantumCounter = 0
+        QuantumCounter = Quantum
 
-        for i in processos.keys():
-        
-            Pn = processos[i][0]
-            
-            Pn -= 1
+        P = Fila.get()[0]
 
-            if(Pn <= 0):
-                remover.append(i)
+        print(P)
+
+        while QuantumCounter > 0:
+
+            print(f"********** TEMPO {tempo} **************")
+            if(Fila.empty):
+                print("FILA: Nao ha processos na fila")
             else:
-                print(f"CPU: {i}({Pn})")
-                processos[i].queue[0] = Pn
+                print("FILA:")
+                for i in range(Fila.qsize):
+                    item = Fila.queue[i]
+                    print("{item}({processos[item][0]})", end=" ")
 
+            IO += 1
 
-        for i in remover:
-            processos[i].get()
-            if(processos[i].empty):
-                processos.pop(i)
+            processoAtual = processos[P][0]
+            
+            print(f"CPU: {P}({processoAtual})")
+
+            processoAtual -= 1
+
+            if(processoAtual == 0):
+                processos[P][0] = 0
+                remover.append(P)
+                break
+            elif(processos[P][2].queue[0] == IO):
+                processos[P][0] = processoAtual
+                Fila.put(P)
+                IO = 0
+                break
+            else:
+                processos[P][0] = processoAtual
+
+            QuantumCounter -= 1
+
+            tempo += 1
+
+            if(tempo == processosFila.queue[0]):
+                Fila.put(processosFila.get())
+        
+        if(QuantumCounter == 0):
+            Fila.put(P)
+
+        removerDic(remover, processos)
         
         remover.clear()
-        tempo += 1
-        
 
-####  apagar os testes abaixo
-
-processos = {
-
-    "P1": [1, 10, queue.Queue()]
-}
-
-processos["P1"][2].put(2)
-processos["P1"][2].put(1)
-
-#processos = readfile(processos)
+processos = readfile()
 
 Quantum = 4
 
-#roundRobin(processos, Quantum)
+roundRobin(processos, Quantum)
